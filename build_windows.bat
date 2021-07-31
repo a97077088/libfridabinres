746f04d33393234ab79960c10355478a086d10f0
@ <@>
2021-07-31 14:29:44 +0800

    build windows

--- build_windows.bat
+++ build_windows.bat
@@ -1,5 +1,5 @@
 
-copy "..\frida_shared\build_windows\frida_shared.dll" ".\frida_shared.dll"
+copy "..\frida_shared\build_windows\frida_shared.dll" ".\frida_shared_windows_arm64.dll"
 git add .
 git commit -m "build windows"
 git push
